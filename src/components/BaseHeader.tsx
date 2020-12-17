3df2482e2e42b839f6af7cbc4954d185a03ae9e5
Alex Rohleder <a.rohleder@sportradar.com>
2020-12-17 15:21:48 +0100

    add first impression

--- src/components/BaseHeader.tsx
+++ src/components/BaseHeader.tsx
@@ -1,7 +1,16 @@
 import "./BaseHeader.scss";
 
 function BaseHeader() {
-  return <header className="base-header">...</header>;
+  return (
+    <header className="base-header">
+      <div className="base-header__meta">
+        <h1>Feed History</h1>
+        <h2>LASK vs. RAPID</h2>
+        <h3>15/09/2020 12:00 UTC</h3>
+        <h4>(sr:match:123456)</h4>
+      </div>
+    </header>
+  );
 }
 
 export default BaseHeader;
